1332eedd0be7e3dbea229af106fb7f591c0b6252
chigozie Francis <franksoft1@yahoo.com>
2021-09-18 09:00:17 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to Franksoft IT Security Consultants under Development
+          <br />
+          <br />
+          <img src={logo} className="App-logo" alt="logo" />
         </a>
       </header>
     </div>
@@ -23,3 +22,9 @@ function App() {
 }
 
 export default App;
+
+{/* 
+Admin URL: https://app.netlify.com/sites/gallant-khorana-3a7eac  
+URL:       https://gallant-khorana-3a7eac.netlify.app
+Site ID:   4d341eb5-79e2-4d1a-bb99-82e16a0f1cfe
+*/}
